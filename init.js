e4e9957a03ecd0a80a30817cb51f6ccc1217f40b
Seba <sebaiova95@gmail.com>
2024-06-16 19:52:24 -0300

    .

--- init.js
+++ init.js
@@ -12,23 +12,23 @@ let palabra;
 let matched = true;
 let is_over = false;
 let times = [];
+
 function next()
 {
     switch(current_step)
     {
         case 0: set_celda(0, celdas_trabajo[0]); break;
         case 1: contar(palabra.length, celdas_trabajo[1]); break;
-        case 2: contar(palabra.length/2, celdas_trabajo[2]); break;
-        case 3: matched = point(celdas_trabajo[0], celdas_trabajo[1]); break;
-        case 4: is_over = mover_puntero(celdas_trabajo[0], celdas_trabajo[1], celdas_trabajo[2]); break;
-        case 5: completed(); break;
-        case 6: escribir_salida(matched); break;
+        case 2: matched = point(celdas_trabajo[0], celdas_trabajo[1]); break;
+        case 3: is_over = mover_puntero(celdas_trabajo[0], celdas_trabajo[1]); break;
+        case 4: completed(); break;
+        case 5: escribir_salida(matched); break;
     }
 
-    if(current_step==3 && !matched)
+    if(current_step==2 && !matched)
         current_step+=2;
-    if(current_step==4 && !is_over)
-        current_step = 2;
+    if(current_step==3 && !is_over)
+        current_step = 1;
 
     current_step++;
 }
@@ -37,11 +37,10 @@ function calc_times()
 {
     times[0] = 1500;
     times[1] = (palabra.length+2)*500;
-    times[2] = ((palabra.length/2)+2)*500;
+    times[2] = 3500;
     times[3] = 3500;
-    times[4] = 3500;
-    times[5] = 1500;
-    times[6] = 0;
+    times[4] = 1500;
+    times[5] = 0;
 }
 
 function play()
@@ -59,34 +58,37 @@ function play()
 function init()
 {
     cinta_trabajo = document.getElementById("cinta_trabajo");
-
-    let button_pĺay = document.getElementById("button_play");
-    button_pĺay.onclick = play;
+    cinta_entrada = document.getElementById("cinta_entrada");
+    cinta_salida = document.getElementById("cinta_salida");
 
     let button_iniciar = document.getElementById("button_iniciar");
     button_iniciar.onclick = iniciar;
-    fill_cinta(celdas_trabajo, cinta_trabajo, "   ");
+    fill_cinta(celdas_trabajo, cinta_trabajo, "  ");
+    fill_cinta(celdas_salida, cinta_salida, "  ");
 }
 
 function iniciar()
 {
-    cinta_entrada = document.getElementById("cinta_entrada");
-    cinta_salida = document.getElementById("cinta_salida");
-
     [...cinta_entrada.children].forEach(c => c.remove());
-    [...cinta_salida.children].forEach(c => c.remove());
     celdas_entrada = [];
-    celdas_salida = [];
+
+    celdas_salida[0].innerText = "";
+    celdas_salida[1].innerText = "";
+    celdas_trabajo[0].innerText = "";
+    celdas_trabajo[1].innerText = "";
 
     matched = true;
     is_over = false;
     current_step = 0;
 
     palabra = document.getElementById("input").value;
-    empty = ' '.repeat(palabra.length);
     fill_cinta(celdas_entrada, cinta_entrada, palabra);
-    fill_cinta(celdas_salida, cinta_salida, empty);
     calc_times();
+
+    setTimeout(function()
+    {
+        play();
+    }, 500);
 }
 
 function fill_cinta(array, cinta, palabra)
@@ -192,7 +194,7 @@ function point(cell1, cell2)
     return letter1==letter2;
 }
 
-function mover_puntero(cell1, cell2, cell3)
+function mover_puntero(cell1, cell2)
 {
     setTimeout(function(){
         cell1.classList.add("yellow");
@@ -218,21 +220,23 @@ function mover_puntero(cell1, cell2, cell3)
         cell2.classList.remove("yellow");
     }, 3000); 
 
-    console.log(cell1.innerText+1);
+    console.log(cell1.innerText + " " + cell2.innerText);
 
-    return (Number(cell1.innerText)+1) == Number(cell3.innerText);
+    return (Number(cell2.innerText) - Number(cell1.innerText)) <= 2;
 }
 
 function completed()
 {
-    setTimeout(function(){
+    setTimeout(function()
+    {
         celdas_trabajo[0].classList.add("correct");
-        celdas_trabajo[2].classList.add("correct");
+        celdas_trabajo[1].classList.add("correct");
     }, 500); 
 
-    setTimeout(function(){
+    setTimeout(function()
+    {
         celdas_trabajo[0].classList.remove("correct");
-        celdas_trabajo[2].classList.remove("correct");
+        celdas_trabajo[1].classList.remove("correct");
     }, 1000); 
 }
 
@@ -240,4 +244,13 @@ function escribir_salida(matched)
 {
     celdas_salida[0].innerText = matched ? "Y" : "N";
     celdas_salida[1].innerText = matched ? "Y" : "O";
+
+    celdas_salida[0].classList.add(matched ? "cyan" : "lila");
+    celdas_salida[1].classList.add(matched ? "cyan" : "lila");
+
+    setTimeout(function()
+    {
+        celdas_salida[0].classList.remove(matched ? "cyan" : "lila");
+        celdas_salida[1].classList.remove(matched ? "cyan" : "lila");
+    }, 500); 
 }
